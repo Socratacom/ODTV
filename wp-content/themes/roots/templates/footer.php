41df2c56e26a265397ab9e5d16f8c2051b942194
Jesse Bufkin <jesse@freshconsulting.com>
2015-02-17 17:29:37 -0800

    Update add this icons to gray.  Link socrata logo in footer

--- wp-content/themes/roots/templates/footer.php
+++ wp-content/themes/roots/templates/footer.php
@@ -83,7 +83,7 @@
       <div class="row">
         <div class="col-md-12">
           <div class="presented">
-            Open Data Tv is Presented By: <img src="<?php echo get_template_directory_uri(); ?>/assets/img/socrata-logo.png" class="img-responsive">
+            Open Data Tv is Presented By: <a href="http://www.socrata.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/socrata-logo.png" class="img-responsive"></a>
           </div>
           <div class="pull-right">
             <?php wp_nav_menu(array('theme_location' => 'footer_navigation', 'items_wrap' => '<ul id="%1$s" class="%2$s list-inline">%3$s</ul>', 'menu_class' => 'footer')); ?>
