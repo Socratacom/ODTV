6d78b8918266e5cd375f5a968dde5e537330305e
Jesse Bufkin <jesse@freshconsulting.com>
2015-02-23 12:16:26 -0800

    Update filter style for mobile.

--- wp-content/themes/roots/front-page.php
+++ wp-content/themes/roots/front-page.php
@@ -4,10 +4,10 @@
 <?php while (have_posts()) : the_post(); ?>
 
   <div class="section-header">
-    <h2><span id="homefilter-text">Featured Videos</span></h2>
     <div class="searchbar">
       <input id="homefilter" class="filter__search js-shuffle-search" type="search" placeholder="Search ODTV">
     </div>
+    <h2><span id="homefilter-text">Featured Videos</span></h2>
   </div>
 <?
 
