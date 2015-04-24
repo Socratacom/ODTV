074c07cc133f0b00e6e3d7c6d4a74cbd07c8e93f
Jesse Bufkin <jesse@freshconsulting.com>
2015-04-24 14:29:13 -0700

    Add data vision section for approval

--- wp-content/themes/roots/front-page.php
+++ wp-content/themes/roots/front-page.php
@@ -4,16 +4,26 @@
 <?php while (have_posts()) : the_post(); ?>
 
   <div class="section-header">
+    <h2>Datavision</h2>
+  </div>
+
+  <?
+   get_template_part('templates/datavision');
+  ?>
+
+  <div class="section-header">
     <div class="searchbar">
       <input id="homefilter" class="filter__search js-shuffle-search" type="search" placeholder="Search ODTV">
     </div>
     <h2><span id="homefilter-text">Featured Videos</span></h2>
   </div>
 <?
-
  get_template_part('templates/featured');
  get_template_part('templates/recent');
+?>
 
+
+<?php
   endwhile;
 ?>
 
---
+++ wp-content/themes/roots/templates/datavision.php
@@ -0,0 +1,71 @@
+<section class="section-results">
+  <div class="row">
+
+  <?php
+    $page_slug = $post->post_name;
+
+    $args = array(
+      'post_type' => 'video',
+      'order' => 'DES',
+      'tax_query' => array(
+        array(
+          'taxonomy'  => 'video-category',
+          'field'   => 'name',
+          'terms'   => 'Datavision',
+        ),
+      ),
+      'posts_per_page' => 100,
+    );
+
+    $loop = new WP_Query( $args );
+    if ( $loop->have_posts() ) :
+      while ( $loop->have_posts() ) : $loop->the_post();
+        $link = get_the_permalink();
+        $title = get_the_title();
+        $post_date = get_the_date();
+        $post_excerpt_quote = get_field('quote');
+        $post_excerpt = get_field('overlay_excerpt');
+        $post_runtime = get_field('run_time');
+        $post_views = '[post_view]';
+
+        $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+        $featured_image = $featured_image[0];
+
+        // field type post object
+        $speaker = get_field('speaker');
+        $speaker_name = $speaker->post_title;
+
+        echo '<div class="item">';
+        echo '<a href="' . $link . '" title="' . $title . '" class="videotile-link" data-backdrop="static">
+                <div class="videotile">
+                  <div class="videotile-content">
+                    <div class="content-img">
+                      <div class="img-container">';
+        echo           '<img src="' . $featured_image . '" class="img-responsive" alt="' . $title . '">
+                      </div>
+                      <div class="img-overlay">
+                        <div class="overlay-text">';
+            echo          '<div class="text-excerpt"><p>' . $post_excerpt . '</p></div>
+                           <div class="text-icons">
+                            <ul class="list list-inline">';
+            echo              '<li><i class="icon-clock"></i> ' . $post_runtime . '<li>';
+            echo              '<li><i class="icon-eye"></i> ' . do_shortcode($post_views) . '<li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="content-text">';
+        echo          '<div class="text-title"><p>' . $title . '</p></div>';
+        echo          '<div class="text-postdate"><p>' . $speaker_name . '</p></div>
+                    </div>
+                  </div>
+                </div>
+              </a>
+              </div>';
+      endwhile;
+
+    endif;
+  ?>
+  </div>
+</section>
